eddcd521aad420fe18655526b13a11454ec06a3e
fahruladitya <69379966+fahruladitya@users.noreply.github.com>
2026-03-29 21:52:33 +0700

    Update Supabase and Sheets sync URLs

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+export const SUPABASE_URL = 'https://fcvftuplkzfpdvvicvpo.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_wLLqVnx9x1IaalxAB0PBXQ_fwI4ycrW'; // anon key
+export const SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbyKWnDrEYIY7oUScoLQV2T0nxVABDnf-zDD5UkDxJubjCD2OMXsQwgMuN8uStJj41F8/exec';
